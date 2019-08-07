4e0b917121214b808c72d102a50143e9484eaff8
Joshua Hill <josh@jlhworkshop.com>
2019-08-07 22:45:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  def name
+    "#{@name}"
+  end
+  
+  def name=(newName)
+    @name = newName
+  end
+  
+  def breed
+  "#{@breed}"
+  end
+  
+  def breed=(newBreed)
+    @breed = newBreed
+  end
+  
+end
